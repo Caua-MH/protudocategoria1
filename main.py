c644de97d214c811978efef0158a74217016adea
ALUNO <c.cornachioni@aluno.ifsp.edu.br>
2024-03-12 21:39:48 -0300

    Criação do requirents.txt

--- main.py
+++ main.py
@@ -1,1 +1,34 @@
+import uuid
 
+from sqlalchemy import create_engine
+from sqlalchemy.orm import DeclarativeBase, relationship
+from sqlalchemy import (Column, Uuid, Integer, String, DataTime, func, DECIMAL, Boolean, ForeignKey)
+
+motor = create_engine("sqlite+pysqlite:///banco_de_dados.sqlite",
+                      echo=True)
+
+class Base(DeclarativeBase):
+    pass
+
+class DatasMixin():
+    dta_cadastro = Column(DataTime, server_default=func.now(), nullable=False)
+    dta_atualizacao = Column(DataTime, onupdate=func.now(), server_default=func.now(), nullable=False)
+
+class Categoria(Base, DatasMixin):
+    __tablename__ = 'tbl_categorias'
+    id = Column(Uuid(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    nome = Column(String(256), nullable=False)
+
+    lista_de_produtos = relationship("Produto", back_populates="categoria",
+                                     cascade="all, delete-orphan", lazy="selection")
+
+class Prduto(Base, DatasMixin):
+    __tablename__ = 'tbl_produtos'
+    id = Column(Uuid(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    nome = Column(String(256), nullable=False)
+    preco = Column(DECIMAL(10, 2), nullable=False)
+    estoque = Column(Integer, default=0)
+    ativo = Column(Boolean, default=False)
+    categoria_id = Column(Uuid(as_uuid=True), ForeignKey('tbl_categorias.id'))
+
+    categoria = relationship("Cat3egoria", back_populates="lista_de_produtos")
